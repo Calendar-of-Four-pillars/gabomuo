f130ef711c6075f57169fa3873c0449d76bdfd43
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2023-02-23 22:21:53 +0900

    feat: 내 정보 조회

--- src/pages/api/users/regist.ts
+++ src/pages/api/users/regist.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth/next';
 import client from 'src/libs/client';
@@ -6,16 +7,51 @@ import { authOptions } from '../auth/[...nextauth]';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
   const session = await getServerSession(req, res, authOptions);
-  const profile = await client.user.findUnique({
-    where: {
-      email: session?.user.email
-    }
-  });
 
-  res.json({
-    ok: true,
-    profile
-  });
+  const {
+    body: {
+      name,
+      gender,
+      birth_year,
+      birth_month,
+      birth_day,
+      birth_hour,
+      birth_minute,
+      is_moon,
+      doubled,
+      region
+    }
+  } = req;
+
+  try {
+    const profile = await client.user.update({
+      where: {
+        email: session?.user.email
+      },
+      data: {
+        name,
+        gender,
+        birth_year,
+        birth_month,
+        birth_day,
+        birth_hour,
+        birth_minute,
+        is_moon,
+        doubled,
+        region
+      }
+    });
+
+    res.json({
+      ok: true,
+      profile
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      error
+    });
+  }
 }
 
-export default withHandler({ method: 'GET', handler, isPrivate: true });
+export default withHandler({ method: 'PUT', handler, isPrivate: true });
