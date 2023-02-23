cc1c481a400d0ab26b96124dd958d99a2e3be111
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2023-02-24 00:32:32 +0900

    fix: 사인인에서 기본정보 기입

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,44 +3,35 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import nextAuth, { NextAuthOptions } from 'next-auth';
 import NaverProvider from 'next-auth/providers/naver';
-import prisma from 'src/libs/client';
+import client from 'src/libs/client';
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(client),
   providers: [
     NaverProvider({
       clientId: process.env.NAVER_ID as string,
-      clientSecret: process.env.NAVER_SECRET as string
-    })
-    // ...add more providers here
-  ],
-  secret: process.env.SECRET,
-  callbacks: {
-    async signIn(data) {
-      if (data.account?.provider === 'naver') {
+      clientSecret: process.env.NAVER_SECRET as string,
+      profile(profile) {
         const {
           response: { gender, birthday, birthyear }
-        } = data.profile as { response: { gender: string; birthday: string; birthyear: string } };
+        } = profile as { response: { gender: string; birthday: string; birthyear: string } };
+
         const convertedForm = {
           gender: gender ?? null,
           birth_year: +birthyear ?? null,
           birth_month: +birthday.split('-')[0] ?? null,
           birth_day: +birthday.split('-')[1] ?? null
         };
-        try {
-          await prisma.user.update({
-            where: { email: data.user.email as string },
-            data: {
-              ...convertedForm
-            }
-          });
-        } catch (error) {
-          return false;
-        }
+        return {
+          ...convertedForm,
+          email: profile.response.email,
+          id: profile.response.id
+        };
       }
-      return true;
-    }
-  }
+    })
+    // ...add more providers here
+  ],
+  secret: process.env.SECRET
 };
 export default nextAuth(authOptions);
