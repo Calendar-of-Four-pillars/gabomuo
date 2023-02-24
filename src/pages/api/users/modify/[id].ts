6f6e69bc9a07419164bb938033d580e071bceb19
peter <secret_wings@naver.com>
2023-02-24 17:49:47 +0900

    fix: 사용하지 않는 import 제거

--- src/pages/api/users/modify/[id].ts
+++ src/pages/api/users/modify/[id].ts
@@ -1,9 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { NextApiRequest, NextApiResponse } from 'next';
-import { getServerSession } from 'next-auth/next';
 import client from 'src/libs/client';
 import withHandler, { ResponseType } from 'src/libs/server/withHandler';
-import { authOptions } from '../../auth/[...nextauth]';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
   const {
