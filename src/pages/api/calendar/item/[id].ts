25ca0f327803ebbc149124dcdb06441d055ecc00
peter <secret_wings@naver.com>
2023-02-26 18:21:11 +0900

    feat: 캘린더 전체 조회

---
+++ src/pages/api/calendar/item/[id].ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from 'pages/api/auth/[...nextauth]';
+import client from 'src/libs/client';
+import withHandler, { ResponseType } from 'src/libs/server/withHandler';
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const session = await getServerSession(req, res, authOptions);
+
+  try {
+    const calendar = await client.calendar.findUnique({
+      where: {
+        id: req.query.id as string
+      }
+    });
+
+    if (session?.id === calendar?.userId) {
+      res.json({
+        ok: false,
+        calendar
+      });
+    }
+
+    if (session?.id !== calendar?.userId) {
+      res.json({
+        ok: false,
+        msg: '조회 권한이 없습니다'
+      });
+    }
+  } catch (error) {
+    res.json({
+      ok: false,
+      msg: '게시글을 찾지 못했습니다.',
+      error
+    });
+  }
+}
+
+export default withHandler({ method: 'GET', handler, isPrivate: true });
