273e69fa5042b2ff7c38519e6e3199f40698b52c
peter <secret_wings@naver.com>
2023-02-26 23:56:48 +0900

    fix: 친구 명조 수정, 삭제 권한 검사 추가

--- src/pages/api/users/delete/[id].ts
+++ src/pages/api/users/delete/[id].ts
@@ -1,16 +1,38 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from 'pages/api/auth/[...nextauth]';
 import client from 'src/libs/client';
 import withHandler, { ResponseType } from 'src/libs/server/withHandler';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const session = await getServerSession(req, res, authOptions);
   try {
+    const exist = await client.friend.findUnique({
+      where: { id: req.query.id as string }
+    });
+    if (!exist) {
+      res.json({
+        ok: false,
+        msg: '없는 데이터 요청입니다.'
+      });
+      return;
+    }
+    if (exist?.userId !== session?.id) {
+      res.json({
+        ok: false,
+        msg: '권한이 없습니다.'
+      });
+      return;
+    }
+
     await client.friend.delete({
       where: { id: req.query.id as string }
     });
 
     res.json({
-      ok: true
+      ok: true,
+      msg: '삭제 완료'
     });
   } catch (error) {
     res.json({
@@ -18,9 +40,6 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
       error
     });
   }
-  res.json({
-    ok: true
-  });
 }
 
 export default withHandler({ method: 'DELETE', handler, isPrivate: true });
--- src/pages/api/users/modify/[id].ts
+++ src/pages/api/users/modify/[id].ts
@@ -1,9 +1,39 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from 'pages/api/auth/[...nextauth]';
 import client from 'src/libs/client';
 import withHandler, { ResponseType } from 'src/libs/server/withHandler';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const session = await getServerSession(req, res, authOptions);
+  try {
+    const exist = await client.friend.findUnique({
+      where: { id: req.query.id as string }
+    });
+
+    if (!exist) {
+      res.json({
+        ok: false,
+        msg: '없는 정보 입니다.'
+      });
+      return;
+    }
+
+    if (exist.userId !== session?.id) {
+      res.json({
+        ok: false,
+        msg: '권한이 없습니다.'
+      });
+      return;
+    }
+  } catch (error) {
+    res.json({
+      ok: false,
+      error
+    });
+    return;
+  }
   const {
     body: {
       name,
@@ -46,9 +76,6 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
       error
     });
   }
-  res.json({
-    ok: true
-  });
 }
 
 export default withHandler({ method: 'PUT', handler, isPrivate: true });
